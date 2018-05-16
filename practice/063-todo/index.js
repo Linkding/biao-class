a42f518814b81ab896b99aca7ec972d6e3460c0f
linkding <beninaoin@gmail.com>
2018-05-16 22:18:00 +0800

    063 to-do 课堂 第二遍

--- practice/063-todo/index.js
+++ practice/063-todo/index.js
@@ -1,90 +1,66 @@
 var max_id = 2;
-var to_do = [
+var todo_list = [
     {
-        id: 1,
-        name: 'lxb',
-        age: 12
+        id : 1,
+        name: 'lxx',
+        age:110
     },
     {
-        id: 2,
-        name : 'bys',
-        age: 122
+        id:2,
+        name:'whh',
+        age:119
     }
 ];
 
-//==========增========
+// ==========增========
 function add(row){
-    var id = ++max_id;
+    var id = max_id + 1;
     row.id = id;
-    to_do.push(row);
+    todo_list.push(row);
 }
 
-add({
-    name: 'bbb',
-    age:112
-})
-
-// console.log(to_do);
-
-//==========删========
+// =========删=========
 function remove(id){
-    var index = find_index_by_id(to_do,id);
-    if(index < 0 ){
-        return;
-    }
-
-    to_do.splice(index,1);
+    var index = find_index_by_id(todo_list,id);
+    todo_list.splice(index,1)
 }
 
-// remove(3);
-// console.log(to_do)
-
-// console.log(to_do)
-
-//==========改=======
+// =========改=======
 function update(id,new_row){
-    //找到需要修改的元素的index
-    var index = find_index_by_id(to_do,id);
-
+    var index = find_index_by_id(todo_list,id);
     if(index < 0)
-        return;
-    var old_row = to_do[index];
-    to_do[index] = Object.assign({},old_row,new_row)
+        return
+    var old_row = todo_list[index];
+    todo_list[index] = Object.assign({},old_row,new_row)
 }
 
-//========查=========
+//========查========
 function read(id){
-    if(id)
-        return find_by_id(to_do,id);
-        // console.log(find_by_id(to_do,1) )
-    return to_do;
-
-}   
-
-
-// console.log(read(3));
-// console.log('findbyid' + find_by_id(to_do,1))
-
-// =======测试区======
-console.log(to_do)
-update(1,{name:'lxx',age:110})
-console.log(to_do);
-
-// ==========功能函数========
-// 通过id找元素
-function find_by_id(arr,id){
-     return arr.find(function(item){
-        return item.id  === id ;
-    });
+    var index = find_index_by_id(todo_list,id)
+    return todo_list[index];
 }
 
-// 通过id找元素索引
+// =======功能方法====
+// 通过id找到元素索引
 function find_index_by_id(arr,id){
-    return arr.findIndex(function(row){
-        // console.log(row.id)
-        // console.log(id)
-        return row.id == id;
+    return arr.findIndex(function(item){
+        return item.id === id;
     })
 }
-
-// console.log(find_index_by_id(to_do,3)) 
+// 通过id找到元素
+function find_by_id(arr,id){
+    return arr.find(function(item){
+        return item.id === id;
+    })
+}
+// =====测试代码======
+// add({name:'test',age:11})
+// console.log(find_index_by_id(todo_list,1))
+// remove(1);
+// console.log(todo_list)
+// console.log(find_by_id(todo_list,1));
+// update(1,{name:'baby',age:11})
+// read(1)
+// console.log(find_index_by_id(todo_list,1))
+// console.log(todo_list[find_index_by_id(todo_list,1)])
+console.log(read(1))
