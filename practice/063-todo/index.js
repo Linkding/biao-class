1713cfe70dd1ef397f3fc41ebd7191e7c96c20eb
linkding <beninaoin@gmail.com>
2018-05-16 22:42:01 +0800

    063 to-do 课堂 第二遍

--- practice/063-todo/index.js
+++ practice/063-todo/index.js
@@ -1,66 +1,8 @@
-var max_id = 2;
-var todo_list = [
-    {
-        id : 1,
-        name: 'lxx',
-        age:110
-    },
-    {
-        id:2,
-        name:'whh',
-        age:119
-    }
-];
-
-// ==========增========
-function add(row){
-    var id = max_id + 1;
-    row.id = id;
-    todo_list.push(row);
+function Todo(){
+    this.todo_list = [];
+    this.add = function (){} 
 }
 
-// =========删=========
-function remove(id){
-    var index = find_index_by_id(todo_list,id);
-    todo_list.splice(index,1)
-}
+var todo = new Todo();
 
-// =========改=======
-function update(id,new_row){
-    var index = find_index_by_id(todo_list,id);
-    if(index < 0)
-        return
-    var old_row = todo_list[index];
-    todo_list[index] = Object.assign({},old_row,new_row)
-}
-
-//========查========
-function read(id){
-    var index = find_index_by_id(todo_list,id)
-    return todo_list[index];
-}
-
-// =======功能方法====
-// 通过id找到元素索引
-function find_index_by_id(arr,id){
-    return arr.findIndex(function(item){
-        return item.id === id;
-    })
-}
-// 通过id找到元素
-function find_by_id(arr,id){
-    return arr.find(function(item){
-        return item.id === id;
-    })
-}
-// =====测试代码======
-// add({name:'test',age:11})
-// console.log(find_index_by_id(todo_list,1))
-// remove(1);
-// console.log(todo_list)
-// console.log(find_by_id(todo_list,1));
-// update(1,{name:'baby',age:11})
-// read(1)
-// console.log(find_index_by_id(todo_list,1))
-// console.log(todo_list[find_index_by_id(todo_list,1)])
-console.log(read(1))
+console.log(todo)
