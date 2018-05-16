31ef39437e940ffa5bfcfb8c942295ee3f062c0e
linkding <beninaoin@gmail.com>
2018-05-17 00:04:25 +0800

    063 to-do 课堂 第三遍

--- practice/063-todo/index.js
+++ practice/063-todo/index.js
@@ -1,8 +1,9 @@
-function Todo(){
-    this.todo_list = [];
-    this.add = function (){} 
+function Todo(name) {
+    this.name = name;
+    this.yo = function () {
+        console.log(`yo  我是${this.name}`)
+    }
 }
 
-var todo = new Todo();
-
-console.log(todo)
+var todo1 = new Todo('whh');
+var todo2 = new Todo('lbb');
