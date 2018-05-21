2b604f6b10e2a6ec14ed1fa3ff174210364faa20
linkding <beninaoin@gmail.com>
2018-05-21 18:23:24 +0800

    复习065 第三遍

--- practice/065-todo-ui-detail/Api.js
+++ practice/065-todo-ui-detail/Api.js
@@ -1,40 +1,65 @@
-function api(max_id){
-  this.max_id = max_id || 101;
-  this.todo_list = [
-    {
+window.Api = api;
 
-    }
+function api(todo,max_id){
+  this.max_id = max_id || 1;
+  this.todo_list = [
+    { // 0
+      id: 100,
+      title: '买菜',
+      remind_at: '2020...',
+      completed: false,
+    },
+    { // 1
+      id: 101,
+      title: '洗菜',
+      remind_at: '2020-10-01 20:20:02',
+      completed: false,
+    },
   ]
-  
-};
+}
 
 api.prototype.add = add;
 api.prototype.remove = remove;
 api.prototype.update = update;
 api.prototype.read = read;
 
-function add(row,id){
+function add(row) {
   var id = this.max_id + 1;
   row.id = id;
   this.todo_list.push(row);
 }
 
-function remove(id){
-  
+function remove(id) {
+  var index_num = find_index_by_id(this.todo_list, id);
+  if(index_num<0)
+    return;
+
+  this.todo_list.splice(index_num, 1);
 }
 
-function update(){
-
+function update(id, new_row) {
+  var index_num = find_index_by_id(this.todo_list, id);
+  if (index_num < 0)
+      return;
+  delete new_row.id;
+  var old_row = this.todo_list[index_num];
+  this.todo_list[index_num] = Object.assign({}, old_row, new_row);
 }
 
-function read(){
-
+function read(id) {
+  if (id)
+      return find_by_id(this.todo_list,id);
+  return this.todo_list
+}
+// 功能函数
+function find_index_by_id(arr, id) {
+  console.log('arr    ' + arr)
+  return arr.findIndex(function (item) {
+      return item.id == id;
+  });
 }
 
-function find_index_by_id(arr,id){
-  arr.forEach(function(item){
-    if(item.id == id){
-      return  arr.findIndex(item.id);
-    }
-  })
+function find_by_id(arr,id) {
+  var index = find_index_by_id(arr, id);
+  return arr[index];
 }
--- practice/065-todo-ui-detail/Ui.js
+++ practice/065-todo-ui-detail/Ui.js
@@ -12,77 +12,29 @@ Ui.prototype.get_form_data = get_form_data;
 Ui.prototype.set_form_data = set_form_data;
 Ui.prototype.render = render;
 Ui.prototype.detect_add = detect_add;
-Ui.prototype.detect_click_list = detect_click_list;
-Ui.prototype.clear_form = clear_form;
+Ui.prototype.detect_click = detect_click;
+Ui.prototype.remove = remove;
 
 function init() {
     this.render();
     this.detect_add();
-    this.detect_click_list();
-}
-
-function detect_add() {
-    var me = this;
-    this.form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        var row = get_form_data(me.form);
-        console.log(row);
-
-        if(!row.title)
-            return;
-
-        if(row.id){
-            me._api.update(row.id,row)
-        } else {
-            me._api.add(row);
-            // console.log(me._api.read())
-        };
-        me.render();
-        me.clear_form();
-        // me.input.value = '';
-    });
-}
-
-function clear_form(){
-   this.form.reset();
-   document.querySelector('')
-}
-
-function detect_click_list() {
-    var me = this;
-    this.list.addEventListener('click', function (e) {
-        var target = e.target
-            , todo_item = target.closest('.todo-item')
-            // , id = parseInt(todo_item.dataset.id)
-            , id = todo_item.dataset.id
-            , is_update_btn = target.classList.contains('update')
-            , is_remove_btn = target.classList.contains('remove')
-            ;
-
-        if (is_update_btn) {
-            var row = me._api.read(id);
-            me.set_form_data(me.form,row);
-        } else if (is_remove_btn) {
-            console.log('点击删除')
-            me._api.remove(id);
-            me.render();
-        }
-    })
+    this.detect_click();
 }
 
 function render() {
-    var todo_list = this._api.read();
     var me = this;
+    //从api获取数据
+    var todo_list = this._api.read();//获取的是一个数组
 
-    this.list.innerHTML = '';
+    //清空历史数据
+    me.list.innerHTML = '';
     todo_list.forEach(function (item) {
         var el = document.createElement('div');
-        el.classList.add('row', 'todo-item')
+        el.classList.add('row', 'todo-item');
         el.dataset.id = item.id;
-
         el.innerHTML = `
         <div class="col checkbox">
-            <input type="checkbox">
+        <input type="checkbox">
         </div>
         <div class="col detail">
             <div class="title">${item.title}</div>
@@ -92,27 +44,75 @@ function render() {
             <button class="remove">删除</button>
         </div>
         `
-            ;
         me.list.appendChild(el);
+    });
+}
+
+function detect_add() {
+    var me = this;
+    this.form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        var row = me.get_form_data(me.form);
+
+        if (row.id) {
+            me._api.update(row.id, row)
+        } else {
+            me._api.add(row)
+        }
+        // 重新渲染
+        me.render();
+        // 清空输入框
+        me.input.value = ''
+    });
+
+}
+
+function detect_click() {
+    var me = this;
+    this.list.addEventListener('click', function (e) {
+        var target = e.target
+            , todo_item = target.closest('.todo-item')
+            , id = todo_item.dataset.id
+            , is_update_btn = target.classList.contains('update')
+            , is_remove_btn = target.classList.contains('remove')
+            ;
+            
+        if (is_update_btn) {
+            var row = me._api.read(id);
+            me.set_form_data(me.form, row);
+        } else if (is_remove_btn) {
+            console.log('id:' + id)
+            me.remove(id);
+        }
     })
+}
+
+function remve(id) {
+    this._api.remove(id);
+    this.render();
 }
 function get_form_data(form) {
     var data = {};
+    // 获取表单内所有拥有name的元素
     var list = form.querySelectorAll('[name]');
-
+    // 遍历name元素
     list.forEach(function (input) {
+        // 按name元素中的节点名分类筛选
         switch (input.nodeName) {
             case 'INPUT':
                 switch (input.type) {
                     case 'text':
                     case 'search':
-                    case 'number':
                     case 'password':
                     case 'hidden':
+                    case 'number':
+                        //如果是用户输入类型，请直接用用户输入的值
                         data[input.name] = input.value;
                         break;
                     case 'radio':
                     case 'checkbox':
+                        // 如果是打钩类数据，使用打钩状态
                         data[input.name] = input.checked;
                         break;
                 }
@@ -125,29 +125,28 @@ function get_form_data(form) {
     return data;
 }
 
+// * @param element form 元素
+// * @param Object data 类{}
 function set_form_data(form, data) {
-    for (var key in data) { //data类型为对象时，遍历的key为键
+    // 遍历对象
+    for (var key in data) {
         var value = data[key];
-
-        var input = form.querySelector(`[name=${key}]`);
-        // 如果不存在指定这个元素name，跳过继续下一个
+        //找到当前属性在表单中的位置
+        var input = document.querySelector(`[name=${key}]`)
+        //如果不存在，则继续下一个
         if (!input)
             continue;
-
-        var data_type = typeof value; //获取值的类型
-        switch (data_type) {
-            // 如果是字符串或者数字，就默认为input[type=text|password|...]
+        var input_type = typeof value;
+        switch (input_type) {
+            /*如果是字符串或者数字，就默认其为input[type=number|text|url|...]*/
             case 'string':
             case 'number':
                 input.value = value;
                 break;
-            // 如果是布尔值，默认input[type=radio|checkbox|...]
+            /*如果是布尔值，就默认其为input[type=radio|checkbox]*/
             case 'boolean':
                 input.checked = value;
                 break;
-
         }
     }
-
 }
-
