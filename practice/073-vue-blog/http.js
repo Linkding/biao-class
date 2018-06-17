960ef9610d35512c84727e2a08acfa84858b05c7
linkding <beninaoin@gmail.com>
2018-06-17 11:21:36 +0800

    重新写一遍 http api

--- practice/073-vue-blog/http.js
+++ practice/073-vue-blog/http.js
@@ -1,26 +1,17 @@
 window.http = {
-    post,
-}
+    post: function (url, data) {
+        data = data || {};
 
-let config = {
-    app_key: 'f9d71547efe93859e344be345859d51c114f6e16234c1c9e49b0f3f281cdfc77',
-    timestamp: (new Date).getTime(),
-};
-config.signature = sign(config.app_key,config.timestamp);
+        data.app_key = 'f9d71547efe93859e344be345859d51c114f6e16234c1c9e49b0f3f281cdfc77';
+        data.timestamp = (new Date).getTime();
+        data.signature = this.sign(data.app_key, data.timestamp)
 
-function post(url,data){
-    url =  'http://mock.biaoyansu.com/api/' + url;
-    let sign = this.sign
-    data = Object.assign({},config,data);
-
-    return axios
-            .post(url,data)
-            .then((res)=>{
+        return axios.post('http://mock.biaoyansu.com/api/' + url, data)
+            .then((res) => {
                 return res;
             })
-}
-
-
-function sign(app_key,datestamp){
-    return btoa(app_key + datestamp)
-}
+    },
+    sign: function (key, timestamp) {
+        return btoa(key + timestamp)
+    }
+}  
