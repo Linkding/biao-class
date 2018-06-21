21460ea71494eee6332ee648a0733068ba5e3831
linkding <beninaoin@gmail.com>
2018-06-21 14:49:45 +0800

    tmp

---
+++ home-practice/linkding-stock/index.js
@@ -0,0 +1,33 @@
+new Vue({
+    el: '.nav',
+    data: {
+        name:'linkding',
+        stock_list: [
+            {
+                name: '中国平安',
+                new_price: '1',
+                change: '1',
+                close: '1',
+                cost: '1',
+                stock_num: '1',
+                mark_value: '1',
+                position: '1',
+                gain_loss: '1',
+            },
+            {
+                name: '中国太保',
+                new_price: '1',
+                change: '1',
+                close: '1',
+                cost: '1',
+                stock_num: '1',
+                mark_value: '1',
+                position: '1',
+                gain_loss: '1',
+            },
+        ]
+    },
+    methods: {
+
+    }
+})
